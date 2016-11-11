e19238abe73420e4c6531abe696a2a82f203e55a
Foo <lcbc.lucascastro@gmail.com>
2016-11-11 15:59:48 -0200

    Implemented

--- sonars.s
+++ sonars.s
@@ -1,7 +1,7 @@
 @ Lucas de Camargo Barros de Castro
 @ RA: 172678
 @
-@ This file implements all functions related to the robot sonars from api_robot2.h
+@ This file implements all functions from api_robot2.h related to the robot sonars
 
 
 @ Reads one of the sonars.
@@ -15,7 +15,7 @@ read_sonar:
         mov r7, #16
         svc 0x0
 
-        add sp, #4              @ Remove parameter from stack
+        add sp, sp, #4          @ Remove parameter from stack
         mov pc, lr
 
 
@@ -31,10 +31,29 @@ read_sonars:
         mov r3, r0              @ Stores the start sonar in r3
 
 loop:
-        bl read_sonar           @ Read sonar for current sonar id
+        bl read_sonar           @ Reads sonar for current sonar id
         str r0, [r2], #4        @ Stores distance in the array
 
         add r3, r3, #1          @ Increments r3 and checks if the loop is done
         mov r0, r3
         cmp r0, r1
         blt loop
+
+        ldmfd sp!, {lr}         @ Recovers lr from stack and returns
+        mov pc, lr
+
+/* Parameters:
+*   r0: id of the sensor that must be monitored.
+*   r1: threshold distance.
+*   r3: address of the function that should be called when the robot gets close to an object.
+* Returns:
+*   void
+*/
+register_proximity_callback:
+        stmfd sp!, {r0-r3}      @ Stacks parameters and makes a syscall
+
+        mov r7, #17
+        svc 0x0
+
+        add sp, sp, #12         @ Removes parameters from stack and return
+        mov pc, lr
