c06639de0ad8de348a438091eab50490453e5798
Foo <lcbc.lucascastro@gmail.com>
2016-11-11 15:59:42 -0200

    Implemented

--- motors.s
+++ motors.s
@@ -1,7 +1,7 @@
 @ Lucas de Camargo Barros de Castro
 @ RA: 172678
 @
-@ This file implements all functions related to the robot motors from api_robot2.h
+@ This file implements all functions from api_robot2 related to the robot motors
 
 
 @ Parameter:
@@ -11,14 +11,14 @@
 @   void
 set_motor_speed:
 
-        ldr r1, [r0, #4]            @ Loads the speed
-        ldr r0, [r0]                @ Loads motor id
+        ldrb r1, [r0, #1]           @ Loads the speed
+        ldrb r0, [r0]               @ Loads motor id
         stmfd sp!, {r0, r1}         @ Stacks parameters for syscall
 
-        mov r7, #18                 @ Set motor speed with a syscall
+        mov r7, #18                 
         svc 0x0
 
-        add sp, #8                  @ Removes parameters from stack and returns
+        add sp, sp, #8              @ Removes parameters from stack and returns
         mov pc, lr
 
 
@@ -29,8 +29,8 @@ set_motor_speed:
 @ Returns:
 @   void
 set_motors_speed:
-        ldr r2, [r0]                @ Loads motors id
-        ldr r3, [r1]
+        ldrb r2, [r0]               @ Loads motors id
+        ldrb r3, [r1]
 
         cmp r2, r3                  @ Checks if r0 contains the motor0 address
         blt endif
@@ -40,12 +40,12 @@ set_motors_speed:
 
 endif:
 
-        ldr r0, [r0, #4]            @ Loads motor0 speed
-        ldr r1. [r1, #4]            @ Loads motor1 speed
+        ldrb r0, [r0, #1]           @ Loads motor0 speed
+        ldrb r1. [r1, #1]           @ Loads motor1 speed
         stmfd sp!, {r0, r1}         @ Stacks parameters for syscall
 
         mov r7, #19                 @ Sets motor speed with syscall
         svc 0x0
 
-        add sp, #8                  @ Removes parameters from stack and returns
+        add sp, sp, #8              @ Removes parameters from stack and returns
         mov pc, lr
